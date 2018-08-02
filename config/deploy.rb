08c8099e67ef6046585a191f3c6a46e22e6445d5
liuyuelintop <liuyuelintop@gmail.com>
2018-08-03 00:18:26 +0800

    Fix App

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,8 +22,7 @@ set :deploy_to, "/home/deploy/blog"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml"
-
+append :linked_files, "config/database.yml", "config/secrets.yml"
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 set :passenger_restart_with_touch, true
